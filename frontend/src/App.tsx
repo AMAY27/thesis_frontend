ceba084b84efd056898a2dfea5f963c4455c178a
Amay Rajvaidya <amay.rajvaidya@s2022.tu-chemnitz.de>
2025-01-04 21:33:47 +0530

    Navigation handler function for nav bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,19 @@
 
 // import './App.css'
+import { NavContextProvider } from './global-context/NavContext';
 import AlertManagement from './pages/Alert-management/AlertManagement'
 import {BrowserRouter as Router , Routes, Route} from 'react-router-dom';
 
 function App() {
 
   return (
-      <Router>
-        <Routes>
-          <Route path="/alerts" element={<AlertManagement />} />
-        </Routes>
-      </Router>
+      <NavContextProvider>
+        <Router>
+          <Routes>
+            <Route path="/alerts" element={<AlertManagement />} />
+          </Routes>
+        </Router>
+      </NavContextProvider>
   )
 }
 
--- frontend/src/components/SideNav/SideNav.tsx
+++ frontend/src/components/SideNav/SideNav.tsx
@@ -1,13 +1,69 @@
 import "../SideNav/SideNav.css";
+import { useNavContext } from "../../global-context/NavContext";
+import { useNavigate } from "react-router-dom";
 
 const SideNav = () => {
+    const { clickedNavItem, setClickedNavItem } = useNavContext();
+    const navigate = useNavigate();
+    
+    const handleNavigation = (navItem: string) => {
+        setClickedNavItem(navItem);
+        // navigate(`/${navItem}`);
+    }
     return(
         <div className="sidenav">
             <ul className="navlist">
-                <li className="navitems">Alerts</li>
-                <li className="navitems">Live Stream</li>
-                <li className="navitems">Custom Events</li>
-                <li className="navitems">Historical Events</li>
+                <li 
+                    className="navitems"
+                    style={{ 
+                        backgroundColor: clickedNavItem === "alerts" ? "#166876" : "transparent",
+                        color: "white",
+                        fontWeight: "bold",
+                        cursor: "pointer"
+                    }}
+                    onClick={() => handleNavigation("alerts")}
+                >
+                    Alerts
+                </li>
+                <li 
+                    className="navitems"
+                    style={{ 
+                        backgroundColor: clickedNavItem === "livestream" ? "#166876" : "transparent",
+                        color: "white",
+                        fontWeight: "bold",
+                        cursor: "pointer"
+                    }}
+                    //style={{ backgroundColor: clickedNavItem === "livestream" ? "#166876" : "transparent" }} 
+                    onClick={() => handleNavigation("livestream")}
+                >
+                    Live Stream
+                </li>
+                <li 
+                    className="navitems"
+                    style={{ 
+                        backgroundColor: clickedNavItem === "customevents" ? "#166876" : "transparent",
+                        color: "white",
+                        fontWeight: "bold",
+                        cursor: "pointer"
+                    }}
+                    //style={{ backgroundColor: clickedNavItem === "customevents" ? "#166876" : "transparent" }} 
+                    onClick={() => handleNavigation("customevents")}
+                >
+                    Custom Events
+                </li>
+                <li 
+                    className="navitems"
+                    style={{ 
+                        backgroundColor: clickedNavItem === "historical" ? "#166876" : "transparent",
+                        color: "white",
+                        fontWeight: "bold",
+                        cursor: "pointer"
+                    }}
+                    //style={{ backgroundColor: clickedNavItem === "historical" ? "#166876" : "transparent" }} 
+                    onClick={() => handleNavigation("historical")}
+                >
+                    Historical Events
+                </li>
             </ul>
         </div>
     )
