2e5dabce937e7520368a934ed7150c56ffcfa6af
Amay Rajvaidya <amay.rajvaidya@s2022.tu-chemnitz.de>
2025-01-06 00:02:29 +0530

    Side nav context update

--- frontend/src/components/SideNav/SideNav.tsx
+++ frontend/src/components/SideNav/SideNav.tsx
@@ -5,7 +5,6 @@ import { useState } from "react";
 
 const SideNav = () => {
     const { clickedNavItem, setClickedNavItem } = useNavContext();
-    const [ hoverClass, setHoverClass ] = useState<Boolean>(false);
     // const navigate = useNavigate();
     
     const handleNavigation = (navItem: string) => {
@@ -13,11 +12,13 @@ const SideNav = () => {
         // navigate(`/${navItem}`);
     }
 
-    function handleHoverClass() {
-        setHoverClass(true);
-    }
+    const navItems = [
+        {item:"alerts", title:"Alerts"}, 
+        {item: "livestream", title:"Live Stream"}, 
+        {item: "customevents", title:"Custom Events"}, 
+        {item: "historical", title:"Historical Events"}
+    ];
 
-    const navItems = [{item:"alerts", title:"Alerts"}, {item: "livestream", title:"Live Stream"}, {item: "customevents", title:"Custom Events"}, {item: "historical", title:"Historical Events"}];
     return(
         <div className="sidenav">
             <ul className="navlist">
@@ -26,66 +27,15 @@ const SideNav = () => {
                         <li
                             key={navItem.item}
                             className={`navitems ${clickedNavItem === navItem.item ? "active" : ""}`}
-                            onMouseEnter={handleHoverClass}
                             onClick={() => handleNavigation(navItem.item)}
                         >
                             {navItem.title}
                         </li>
                     )
                 })}
-                {/* <li 
-                    className="navitems"
-                    style={{ 
-                        backgroundColor: clickedNavItem === "alerts" ? "#166876" : "transparent",
-                        color: "white",
-                        fontWeight: "bold",
-                        cursor: "pointer"
-                    }}
-                    onClick={() => handleNavigation("alerts")}
-                >
-                    Alerts
-                </li>
-                <li 
-                    className="navitems"
-                    style={{ 
-                        backgroundColor: clickedNavItem === "livestream" ? "#166876" : "transparent",
-                        color: "white",
-                        fontWeight: "bold",
-                        cursor: "pointer"
-                    }}
-                    //style={{ backgroundColor: clickedNavItem === "livestream" ? "#166876" : "transparent" }} 
-                    onClick={() => handleNavigation("livestream")}
-                >
-                    Live Stream
-                </li>
-                <li 
-                    className="navitems"
-                    style={{ 
-                        backgroundColor: clickedNavItem === "customevents" ? "#166876" : "transparent",
-                        color: "white",
-                        fontWeight: "bold",
-                        cursor: "pointer"
-                    }} 
-                    onClick={() => handleNavigation("customevents")}
-                >
-                    Custom Events
-                </li>
-                <li 
-                    className="navitems"
-                    style={{ 
-                        backgroundColor: clickedNavItem === "historical" ? "#166876" : "transparent",
-                        color: "white",
-                        fontWeight: "bold",
-                        cursor: "pointer"
-                    }}
-                    //style={{ backgroundColor: clickedNavItem === "historical" ? "#166876" : "transparent" }} 
-                    onClick={() => handleNavigation("historical")}
-                >
-                    Historical Events
-                </li> */}
             </ul>
         </div>
     )
 }
 
-export default SideNav
+export default SideNav;
