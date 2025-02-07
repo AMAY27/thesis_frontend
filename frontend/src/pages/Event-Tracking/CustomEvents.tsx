503e2f010bce130a97d6ab74db099cb323ebafaf
Amay Rajvaidya <amay.rajvaidya@s2022.tu-chemnitz.de>
2025-02-07 14:05:51 +0530

    Custom events graph implementation

--- frontend/src/pages/Event-Tracking/CustomEvents.tsx
+++ frontend/src/pages/Event-Tracking/CustomEvents.tsx
@@ -5,6 +5,8 @@ import GlobalForm from "../../components/Forms/GlobalForm";
 import { BaseEventProps } from "../../components/Forms/GlobalForm";
 import { CustomEventProps, CustomEventAnalyticsProps } from './types';
 import { getCustomEvents, getCustomEventAnalytics } from './api.service';
+import {Bar, BarChart, Tooltip, XAxis, YAxis, ResponsiveContainer} from 'recharts';
+import { DailyFrequencyDto } from './types';
 import './CustomEvents.css'
 
 const CustomEvents = () => {
@@ -14,6 +16,8 @@ const CustomEvents = () => {
     const [customEvents, setCustomEvents] = React.useState<CustomEventProps[]>([]);
     const [customEventAnalytics, setCustomEventAnalytics] = React.useState<CustomEventAnalyticsProps>();
     const [graphFormat, setGraphFormat] = React.useState<string>("monthly");
+    const [monthForDailyAnalytics, setMonthForDailyAnalytics] = React.useState<string>("");
+    const [dailyFrequency, setDailyFrequency] = React.useState<DailyFrequencyDto[]>([]);
 
     useEffect(() => {
       const handleResize = () => {
@@ -42,9 +46,29 @@ const CustomEvents = () => {
 
     const handleEventSelect = async (e: React.ChangeEvent<HTMLSelectElement>) => {
       if(e.target.value) {
-        const resp:CustomEventAnalyticsProps = await getCustomEventAnalytics("/custom-events/getEventAnalytics", "676bdb353db50d80a5c8a82a", e.target.value);
+        const resp:CustomEventAnalyticsProps = await getCustomEventAnalytics(
+          "/custom-events/getEventAnalytics", 
+          "676bdb353db50d80a5c8a82a", 
+          e.target.value
+        );
         setCustomEventAnalytics(resp);
+        if (resp.frequencies && resp.frequencies.length > 0) {
+          setMonthForDailyAnalytics(resp.frequencies[0].month);
+          setDailyFrequency(resp.frequencies[0].dailyFrequency);
+        }
+        console.log(customEventAnalytics)
       }
+    }
+
+    const handleMonthChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+      setMonthForDailyAnalytics(e.target.value);
+      setDailyFrequency(customEventAnalytics?.frequencies?.find((freq) => freq.month === e.target.value)?.dailyFrequency ?? []);
+      console.log(dailyFrequency);
+    }
+
+    const handleDailyClicked = () => {
+      setGraphFormat("daily");
+      setDailyFrequency(customEventAnalytics?.frequencies[0]?.dailyFrequency ?? []);
     }
 
 
@@ -66,7 +90,46 @@ const CustomEvents = () => {
         </div>
         <div className='graph-format-selector'>
           <div className={`${graphFormat  === 'monthly' ? 'format-active': ''}`} onClick={() => setGraphFormat("monthly")}>Monthly</div>
-          <div className={`${graphFormat  === 'daily' ? 'format-active': ''}`} onClick={() => setGraphFormat("daily")}>Daily</div>
+          <div className={`${graphFormat  === 'daily' ? 'format-active': ''}`} onClick={handleDailyClicked}>Daily</div>
+        </div>
+        <div className='custom-events-graph'>
+          {graphFormat === 'monthly' && customEventAnalytics?.frequencies && (
+            <ResponsiveContainer width="100%" height={400}>
+              <BarChart width={730} height={250} 
+              data={customEventAnalytics.frequencies.map((freq) => ({
+                  month: freq.month,
+                  count: freq.freq
+              }))}>
+                <XAxis dataKey="month" />
+                <YAxis dataKey="count"/>
+                <Tooltip />
+                <Bar dataKey="count" fill="#62B2C0" />
+                </BarChart>
+              </ResponsiveContainer>
+            )}
+          {graphFormat === 'daily' && customEventAnalytics?.frequencies && (
+            <>
+              <div className='day-selector'>
+                <select name="" id="" value={monthForDailyAnalytics} onChange={handleMonthChange}>
+                  {customEventAnalytics?.frequencies?.map((freq) => {
+                    return (
+                      <option value={freq.month} key={freq.month}>{freq.month}</option>
+                    )
+                  })}
+                </select>
+              </div>
+              {dailyFrequency && dailyFrequency.length > 0 && (
+                <ResponsiveContainer width="100%" height={400}>
+                  <BarChart width={730} height={250} data={dailyFrequency}>
+                    <XAxis dataKey="date" />
+                    <YAxis dataKey="count"/>
+                    <Tooltip />
+                    <Bar dataKey="count" fill="#62B2C0" />
+                  </BarChart>
+                </ResponsiveContainer>
+              )}
+            </>
+          )}
         </div>
       </div>
       <div className='custom-events-right-container'>
